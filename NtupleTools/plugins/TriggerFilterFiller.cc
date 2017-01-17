2f7f8c69fe896c66f4f3580f3b48fdb8bf038249
Laura Dodd <ldodd@cern.ch>
2017-01-17 11:10:21 -0600

    adding files

---
+++ NtupleTools/plugins/TriggerFilterFiller.cc
@@ -0,0 +1,7 @@
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "MonoHTauTau/NtupleTools/plugins/TriggerFilterFiller.h"
+
+DEFINE_EDM_PLUGIN(NtupleFillerFactory, TriggerFilterFiller, "TriggerFilterFiller");
+
+
